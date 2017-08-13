5de04f6aef54bd0cb53daeae6c59f6c5d8f1a786
Hemlata Pathak <hemlata.p@hcl.com>
2017-08-13 18:30:00 +0530

    updated home pageobject repository

---
+++ src/test/java/com/trivago/pages/BlogPage.java
@@ -0,0 +1,13 @@
+package com.trivago.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.trivago.configpageobjects.GetPage;
+
+public class BlogPage extends GetPage {
+	
+	public BlogPage(WebDriver driver) {
+		super(driver, "BlogPage");
+	}
+	
+}
